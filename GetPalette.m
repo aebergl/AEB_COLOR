79b7768270d740aafc5d77932c5c992fd1d004fe
aebergl <aebergl@gmail.com>
2021-08-30 12:24:42 -0400

    Included Tab

--- GetPalette.m
+++ GetPalette.m
@@ -10,7 +10,7 @@ function [palette] = GetPalette( varargin )
 %   Current valid palettes are: 'JCO','nejm','Lancet','Science','Nature',
 %   'lines','Colors','aeb01','ETS01','ETS02','Yun_LAML,'TCGA33'
 
-ValidPalettIds={'JCO','nejm','Lancet','Science','Nature','lines','Colors','aeb01','ETS01','ETS02','Yun_LAML','TCGA33'};
+ValidPalettIds={'Tab10','Tab20','JCO','nejm','Lancet','Science','Nature','lines','Colors','aeb01','ETS01','ETS02','Yun_LAML','TCGA33'};
 
 if nargin < 1 || ~any(strcmpi(varargin{1},[ValidPalettIds,'GetPaletteIds']) )
     palette = ValidPalettIds;
@@ -20,6 +20,41 @@ else
     Id = varargin{1};
     if strcmpi(Id,'GetPaletteIds')
         palette = ValidPalettIds;
+     elseif strcmpi(Id,'Tab10')
+        palette = [
+            0.1216    0.4667    0.7059
+            1.0000    0.4980    0.0549
+            0.1725    0.6275    0.1725
+            0.8392    0.1529    0.1569
+            0.5804    0.4039    0.7412
+            0.5490    0.3373    0.2941
+            0.8902    0.4667    0.7608
+            0.4980    0.4980    0.4980
+            0.7373    0.7412    0.1333
+            0.0902    0.7451    0.8118];
+    elseif strcmpi(Id,'Tab20')
+        palette = [
+            0.1216    0.4667    0.7059
+            0.6824    0.7804    0.9098
+            1.0000    0.4980    0.0549
+            1.0000    0.7333    0.4706
+            0.1725    0.6275    0.1725
+            0.5961    0.8745    0.5412
+            0.8392    0.1529    0.1569
+            1.0000    0.5961    0.5882
+            0.5804    0.4039    0.7412
+            0.7725    0.6902    0.8353
+            0.5490    0.3373    0.2941
+            0.7686    0.6118    0.5804
+            0.8902    0.4667    0.7608
+            0.9686    0.7137    0.8235
+            0.4980    0.4980    0.4980
+            0.7804    0.7804    0.7804
+            0.7373    0.7412    0.1333
+            0.8588    0.8588    0.5529
+            0.0902    0.7451    0.8118
+            0.6196    0.8549    0.8980];
+
     elseif strcmpi(Id,'JCO')
         palette = [
             0                   0.450980392156863   0.760784313725490
